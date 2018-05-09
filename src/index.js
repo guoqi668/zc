8572c29fb66fdde398f7215fbb60ce8310410b8e
destinyfors <destinyfors@gamil.com>
2018-05-09 13:56:48 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+const App = () => (<h1 style={{textAlign: 'center'}}>支撑平台移动端</h1>)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
